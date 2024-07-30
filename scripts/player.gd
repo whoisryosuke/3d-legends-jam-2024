3481b13b98dbd64c3ed14baed77838e8c4d0702e
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-07-29 17:12:20 -0700

    :tada: 3D + godot oss mesh + mesh lib

---
+++ scripts/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var model = $GobotSkin
+
+# Local Variables
+var speed = SPEED
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	if Input.is_action_pressed("Sprint"):
+		speed = SPEED * 2
+	else:
+		speed = SPEED
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("Move Left", "Move Right", "Move Forward", "Move Backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	# Change animation
+	if (direction.x != 0 or direction.z != 0) and is_on_floor():
+		model.run()
+	elif direction.x == 0 and direction.z == 0 and is_on_floor():
+		model.idle()
+	elif !is_on_floor():
+		model.jump()
+
+	move_and_slide()
